a0f6f9d8a760345da65fa1e9227ac783e7c3ddc9
jackyfox <fox.sawyer@gmail.com>
2011-04-20 16:09:52 +0400

    Highcharts extention added

--- protected/views/gasAlarm/_tests.php
+++ protected/views/gasAlarm/_tests.php
@@ -3,5 +3,5 @@
 	<div class="date">Дата: <?php echo date("d.m.Y", strtotime($test->date)); ?></div>
 	<div class="temp">Температура: <?php echo $test->temperature; ?></div>
 	<div class="humi">Влажность: <?php echo $test->humidity; ?></div>
-</div><!-- test -->
+</div><!-- test --><br />
 <?php endforeach; ?>
